01001ea133baf80b17436e7fcaa5432104ad2fb3
Kenneth Du <kennethdu3@gmail.com>
2018-08-17 14:45:33 -0700

    once again put placeholder javascript to hopefully make it functioning with proper variables and logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
-
 var translation = function(inputted){
 	
 	var splitInput = inputted.split('');
 	var result = [];
+	var translatedResults;
 		
 	splitInput.forEach(function(number) {
-		var trans = ("");
-			if (parseInt(splitInput[i]) % 3 === 0 && parseInt(splitInput[i] !== 0)) {
-			trans =("I'm sorry, Dave. I'm afraid I can't do that.");
-			}else{
-				result.push(splitInput[i]);
+		var trans = '';
+			if (parseInt(number % 3 === 0 && parseInt(number !== 0)) {
+				trans = "I'm sorry, Dave. I'm afraid I can't do that.";
+			}else if (number === "0") {
+				trans = "Beep!";
+			}else if (number === "1") {
+				trans = "Boop!";
+			}else{			
+				trans= number;
 			}	
+			translatedResults.push(trans);
 	});
-		result.push(trans);
 };
 
 	
 
 $(document).ready(function(){
 
-  $("form#formOne").submit(function(event){
-  event.preventDefault();
-	// var result = ("");
-	var userInput = $("#textInput").val();
-	 
-  translation(userInput);
-  });
+	$("form#formOne").submit(function(event){
+		event.preventDefault();
+		// var result = ("");
+		var userInput = $("#textInput").val();
+		 
+		translatedResults = translation(userInput);
+	});
 
 
 });
