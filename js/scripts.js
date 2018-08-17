5238e8bb92c448761ed01dc930c1a33287b844d4
Kenneth Du <kennethdu3@gmail.com>
2018-08-17 10:56:40 -0700

    add working 'I'm sorry, Dave. I'm afraid I can't do that' text to all numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,28 @@ $(document).ready(function(){
 
   $("form#formOne").submit(function(event){
   event.preventDefault();
-  // var result = ("");
-  var userInput =($("#textInput").val());
-  var splitInput = userInput.split("");
-  $("#result").text(transNumber);
+	// var result = ("");
+	var userInput = $("#textInput").val();
+	var splitInput = userInput.split("");
+	 
+  $("#result").text(translation);
   });
 
 
 });
 
-var numbers = ["2", "4", "5", "6", "7", "8", "9"];
+// var numbers = ["2", "4", "5", "6", "7", "8", "9"];
+// var beep = ["0"];
 
 var translation = function(number) {
-	for (i=0 ; splitInput.length; i++)
-		if splitInput[i]===("0");
-		splitInput[i]===("Beep!");
-}
+	if (number %3===0) {
+		return ("I'm sorry, Dave. I'm afraid I can't do that.")
+	};
+	
+	// for (i=0 ; i < splitInput.length; i++) {
+		// if (beep.indexOf(splitInput[i]<= "0")) {
+			// splitInput[i] === "Beep!";
+		// };
+		// return splitInput
+	// };
+};
