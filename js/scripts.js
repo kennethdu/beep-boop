f058378bd62d66770c0d5467f43b05089310e25b
Kenneth Du <kennethdu3@gmail.com>
2018-08-17 11:57:23 -0700

    working on logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
-$(document).ready(function(){
-
-  $("form#formOne").submit(function(event){
-  event.preventDefault();
-	// var result = ("");
-	var userInput = $("#textInput").val();
-	var splitInput = userInput.split("");
-	 
-  $("#result").text(translation);
-  });
-
-
-});
 
 // var numbers = ["2", "4", "5", "6", "7", "8", "9"];
 // var beep = ["0"];
 
 var translation = function(number) {
-	if (number %3===0) {
-		return ("I'm sorry, Dave. I'm afraid I can't do that.")
-	};
+var userInput = $("#textInput").val();
+var splitInput = userInput.split();
+	
+	for (i=0 ; i < splitInput.length ; i++) {
+			
+		if (splitInput.length[i] % 3 === 0) {
+			return ("I'm sorry, Dave. I'm afraid I can't do that.");
+			}else {
+				return false;
+			}
+	}
+}
 	
 	// for (i=0 ; i < splitInput.length; i++) {
 		// if (beep.indexOf(splitInput[i]<= "0")) {
@@ -26,4 +22,18 @@ var translation = function(number) {
 		// };
 		// return splitInput
 	// };
-};
+
+
+
+
+$(document).ready(function(){
+
+  $("form#formOne").submit(function(event){
+  event.preventDefault();
+	// var result = ("");
+	 
+  $("#result").text(translation);
+  });
+
+
+});
