1557d68467e17020ceb56fd528c03aaded861b96
Kenneth Du <kennethdu3@gmail.com>
2018-08-17 10:10:45 -0700

    add semi function javascript

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $("form#formOne").submit(function(event){
+  event.preventDefault();
+  // var result = ("");
+  var userInput =($("#textInput").val());
+  var splitInput = userInput.split("");
+  $("#result").text(transNumber);
+  });
+
+
+});
+
+var numbers = ["2", "4", "5", "6", "7", "8", "9"];
+
+var translation = function(number) {
+	for (i=0 ; splitInput.length; i++)
+		if splitInput[i]===("0");
+		splitInput[i]===("Beep!");
+}
