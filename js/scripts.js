5b3ef31a496b38d5b4c64c07d75af6c51b053cba
Kenneth Du <kennethdu3@gmail.com>
2018-08-17 13:44:56 -0700

    working on new variables to make working javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,28 @@
 
-// var numbers = ["2", "4", "5", "6", "7", "8", "9"];
-// var beep = ["0"];
-
 var translation = function(number) {
-var userInput = $("#textInput").val();
-var splitInput = userInput.split();
+	
+var splitInput = number.split("");
+var result = [];
 	
 	for (i=0 ; i < splitInput.length ; i++) {
 			
-		if (splitInput.length[i] % 3 === 0) {
-			return ("I'm sorry, Dave. I'm afraid I can't do that.");
-		}else if (splitInput.length[i] === 1) {
-			return ("Boop!"); 
-		}else if (splitInput.length[i] === 0) {
-			return ("Beep!");
-			}else {
-				return false;
-			}
+		if (parseInt(splitInput[i]) % 3 === 0 && parseInt(splitInput[i] !== 0)) {
+		result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+		}else{
+			result.push(splitInput[i]);
 	}
 }
+}
 	
-	// for (i=0 ; i < splitInput.length; i++) {
-		// if (beep.indexOf(splitInput[i]<= "0")) {
-			// splitInput[i] === "Beep!";
-		// };
-		// return splitInput
-	// };
-
-
-
 
 $(document).ready(function(){
 
   $("form#formOne").submit(function(event){
   event.preventDefault();
 	// var result = ("");
+	var userInput = $("#textInput").val();
 	 
-  $("#result").text(translation);
+  translation(userInput);
   });
 
 
