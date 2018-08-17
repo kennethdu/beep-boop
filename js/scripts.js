8c0f57412ed9449c55f3f0cec43b9f0d9a0f4364
Kenneth Du <kennethdu3@gmail.com>
2018-08-17 14:20:51 -0700

    refactor code to get rid of errors. working on working javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 
-var translation = function(number) {
+var translation = function(inputted){
 	
-var splitInput = number.split("");
-var result = [];
-	
-	for (i=0 ; i < splitInput.length ; i++) {
-			
-		if (parseInt(splitInput[i]) % 3 === 0 && parseInt(splitInput[i] !== 0)) {
-		result.push("I'm sorry, Dave. I'm afraid I can't do that.");
-		}else{
-			result.push(splitInput[i]);
-	}
-}
-}
+	var splitInput = inputted.split('');
+	var result = [];
+		
+	splitInput.forEach(function(number) {
+		var trans = ("");
+			if (parseInt(splitInput[i]) % 3 === 0 && parseInt(splitInput[i] !== 0)) {
+			trans =("I'm sorry, Dave. I'm afraid I can't do that.");
+			}else{
+				result.push(splitInput[i]);
+			}	
+	});
+		result.push(trans);
+};
+
 	
 
 $(document).ready(function(){
