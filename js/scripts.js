ecc70ae0809f132ee72d4c8f2f08615321fe2ad0
Kenneth Du <kennethdu3@gmail.com>
2018-08-21 22:10:13 -0700

    add working javascript, still need to work on reset field function

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,15 @@
-var translation = function(input) {
-	
-	var splitInput = input.split(); 
-	var result = [];
-	var translatedResults;
-	
-		
-	splitInput.forEach(function(number) {
-		var translate = '';
-			if (parseInt(number)% 3 === 0 && parseInt(number) > 0) {
-				translate = "I'm sorry, Dave. I'm afraid I can't do that.";
-			} else if (number.includes("1")) {
-			translate = "Boop!";
-			} else if (number.includes("0")) {
-				translate = "Beep!";
-			// } else{
-				// var range = function (start, number) {
-					// for (i = 0 ; i <= number ; i++) {
-						// result.push[i];
-					
-				// translate = range;
-					// }
-				// };	
-			// result.push(translate);
-			// }; working on showing a range of numbers code not operationtional
-	translatedResults = transString(result);
-	return translatedResults;
-	});
-};
-
-var transString = function (transInput) {
-	translatedResults = transInput.join("");
-	return translatedResults;
+var translatedResults = function(number) {
+	for (i = 0 ; i <= number ; i++) {
+		if (i % 3 === 0 && i !== 0) {
+			$(".result").append("Sorry Dave!");
+		}else if (i.toString().includes("1")){
+			$(".result").append("Boop!");
+		}else if (i.toString().includes("0")){
+			$(".result").append("Beep!");
+		}else {
+			$(".result").append(i);
+		}
+	};	
 };
 
 	
@@ -38,8 +17,9 @@ var transString = function (transInput) {
 $(document).ready(function(){
 	$("form#formOne").submit(function(event){
 		event.preventDefault();
-		var userInput = $("#textInput").val();		 
-		translatedResults = translation(userInput);
-		$("#result").text(translatedResults);
+		var userInput = $("#textInput").val();
+		var userNumber = parseInt(userInput);
+		var translate = translatedResults(userNumber);
+		$("#result").append(" " + translate + " ");
 	});
 });
